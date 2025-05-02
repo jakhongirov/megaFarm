50488f2c3b98ffc7f9187fe9a798fd3cf27433de
jakhongirov <johongirovdiyorbek08@gmail.com>
2025-05-02 19:09:56 +0800

    add

--- src/lib/postgres.js
+++ src/lib/postgres.js
@@ -3,7 +3,7 @@ const { Pool } = require("pg");
 const credentials = {
    user: "postgres",
    host: "localhost",
-   database: "xisobchiai",
+   database: "megaFarm",
    password: "behad2024",
    port: 5432,
    timezone: 'Asia/Tashkent'
